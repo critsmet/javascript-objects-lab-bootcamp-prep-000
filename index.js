803f1671ba2cfcfe36ae2312ee48090f1d4aae8f
Christopher Metzger <chrismetzgr@gmail.com>
2018-04-10 16:07:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
    object[key] = value; 
    return object
 }
-function deleteFromObjectsByKey(object,key) {
+function deleteFromObjectByKey(object,key) {
   var newObj = Object.assign({}, object)
   delete newObj.key
   return newObj
