2c6d33fd7e713a154bdc79bf094d7e03588e2922
Christopher Metzger <chrismetzgr@gmail.com>
2018-04-10 15:32:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign(object, { [key]: value });
 }
 function updateObjectWithKeyAndValue(object, key, value) {
-  Object.assign({}, { [key]: value });
+  Object.assign({}, object { [key]: value });
 }
