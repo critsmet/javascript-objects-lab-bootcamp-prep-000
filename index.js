bb3a848b913c56eaa9ed3488692161e2aa578b6d
Christopher Metzger <chrismetzgr@gmail.com>
2018-04-10 15:19:27 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,7 @@
+var recipes = {};
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign(object, { [key]: value });
+}
+function updateObjectWithKeyAndValue(object, key, value) {
+  var 2 = recipes
+}
