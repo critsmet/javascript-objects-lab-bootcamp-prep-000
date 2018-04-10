965e3ec1cd8d9addc425bd5b2911084101309b78
Christopher Metzger <chrismetzgr@gmail.com>
2018-04-10 16:27:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ function deleteFromObjectByKey(object,key) {
 }
 }
 function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object.key
-  return object
+  var newObject = delete object.key
+  return newObject
 }
